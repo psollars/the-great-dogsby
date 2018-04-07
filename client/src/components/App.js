0602f9408e541513b936cb8cd9dc0ccc8c16fec4
Patrick Sollars <pjsollars@gmail.com>
2018-04-07 10:38:22 -0400

    add state and elements

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,17 +2,35 @@ import React, { Component } from 'react';
 import '../assets/styles/App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      dogImage: 'https://dog.ceo/api/img/germanshepherd/n02106662_6966.jpg'
+    }
+  }
+
   render() {
     return (
-      <div className="App">
+      <div className='App'>
         <header>
           <h1>The Great Dogsby</h1>
         </header>
-        <section className="main-content">
+        <section className='main-content'>
+          <div>
+            <img className='dog-picture' src={this.state.dogImage} />
+          </div>
+          <div>
+            <button onClick={this.fetch}>Fetch boy, fetch!</button>
+          </div>
         </section>
       </div>
     );
   }
-}
+
+  fetch = () => {
+    alert('good boy');
+  }
+
+} // end of component
 
 export default App;
